9c23be15b21ba636efb3f0b1174655611181c4f3
YinhuC <yinhucui@hotmail.com>
2020-04-02 18:39:43 +1300

    Initial files

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+/* Third Party */
+import React from "react";
+
+/* Components */
+import MarketingPage from "./MarketingPage";
+
+/* Functions */
+
+class Main extends React.Component {
+  render() {
+    return <>Hello</>;
+  }
+}
+export default Main;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./Main";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const element = (
+  <>
+    <Main />
+  </>
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+console.log(element);
+ReactDOM.render(element, document.getElementById("root"));
