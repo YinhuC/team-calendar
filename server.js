c7a2272ab4dc59607da240c5e1b5326992fee9b8
Hideaki Murakami <thehiderofkeys@gmail.com>
2020-05-11 11:00:56 +1200

    Add local server as proxy in development
    Server will serve client build in production
    
    Resolves: #22

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 import express from "express"
+import path from 'path';
 
 const app = express();
 const port = process.env.PORT || 10000;
 
-app.get("/", (req,res) => {res.send("Hello World!");});
+app.get("/api", (req,res) => {res.send("Hello World!");});
+
+if (process.env.NODE_ENV === 'production') {
+    console.log("Running in production!");
+    app.use(express.static(path.join(__dirname, "client/build")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
+
 
 app.listen(port, ()=>{console.log(`Listening on port ${port}!`)})
