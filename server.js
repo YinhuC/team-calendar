7eeaccddf4b29b907835c8a558208a3c1573e319
Hideaki Murakami <thehiderofkeys@gmail.com>
2020-05-10 18:27:55 +1200

    Add dummy entry point for server
    See: #22

---
+++ server.js
@@ -0,0 +1,8 @@
+import express from "express"
+
+const app = express();
+const port = process.env.PORT || 10000;
+
+app.get("/", (req,res) => {res.send("Hello World!");});
+
+app.listen(port, ()=>{console.log(`Listening on port ${port}!`)})
