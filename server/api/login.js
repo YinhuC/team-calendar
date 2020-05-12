ad05a14bf6cdaf0d47cc7cfa1a3c347e7b35a919
Soumith Chittajallu <soumithc@gmail.com>
2020-05-12 18:21:39 +1200

    create a new user on login

--- server/api/login.js
+++ server/api/login.js
@@ -1,9 +1,11 @@
 const {google} = require('googleapis')
 import fs from 'fs'
+import { User } from '../db/schema'
 
 const cred = JSON.parse(fs.readFileSync('./credentials.json'))
 const authScope = [
     'https://www.googleapis.com/auth/userinfo.profile',
+    'https://www.googleapis.com/auth/userinfo.email',
     'https://www.googleapis.com/auth/calendar.readonly',
 ]
 const oAuth2Client = new google.auth.OAuth2(
@@ -24,12 +26,27 @@ export default router => {
             var oauth2 = google.oauth2({auth: oAuth2Client,version: 'v2'})
             oauth2.userinfo.get((err, userRes) => {
                 req.session.user = userRes.data;
-                console.log(req.session.user);
+                createUser(token, userRes.data);
                 process.env.NODE_ENV === 'production' ? res.redirect('/landing') :res.redirect('http://localhost:3000/landing')
             });
+            //createUser(token, req.session.user)
         });
     }) 
     router.get("/user_details", (req, res) => {
-        res.json(req.session.user)
+        res.json(req.session.user);
+        console.log(req.session.user);
     })
+}
+
+async function createUser(token, userData) {
+    const newUser = new User();
+    newUser.accessToken = token.access_token;
+    newUser.googleId = userData.id;
+    newUser.firstName = userData.given_name;
+    newUser.lastName = userData.family_name;
+    newUser.email = userData.email;
+    newUser.groups = [];
+
+    await newUser.save();
+    console.log(`New user saved! _id = ${newUser._id}`)
 }
--- server/db/schema.js
+++ server/db/schema.js
@@ -3,8 +3,8 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    googleId: { type: String, unique: true },
     accessToken: String,
+    googleId: { type: String, unique: true },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true },
