d9dded8d906ce55c4285e87a4457ad9b3077a867
Soumith Chittajallu <soumithc@gmail.com>
2020-05-13 20:18:02 +1200

    Define add override CRUD operations for groups
    still a WIP, this provides a starting point. CRUD methods may need to be updated later

--- server/api/groups.js
+++ server/api/groups.js
@@ -10,15 +10,48 @@ router.use('/groups', mongooseCrudify({
     beforeActions: [
         {
             middlewares: [ensureLogin]
+        },
+        {
+            middlewares: [isMember],
+            except: ['create']
+        }
+    ], 
+    actions: {
+        read: (req, res) => {
+            Group.populate(req.crudify.group, 'members').then(() => res.json(req.crudify.group));
+        }
+    },
+    afterActions: [
+        {
+            middlewares: [logToConsole]
         }
     ]
 }));
 
-function ensureLogin(req, res) {
+function ensureLogin(req, res, next) {
     if (req.session.user) {
         next();
     } else {
         return res.sendStatus(401);
     }
 }
+
+function isMember(req, res, next) {
+    const isMember = Group.find({}).populate({
+        path: 'members',
+        match: { googleId: req.session.user.id }
+    }).exec();
+    if (isMember) {
+        next();
+    } else {
+        return res.sendStatus(401);
+    }
+}
+
+function logToConsole(req, res, next) {
+    console.log(req.crudify.err || req.crudify.result);
+    next();
+}
+
+export default router;
 
