ebfce2cb3c3dd3daa29f2564fb2473c53665f3a4
YinhuC <yinhucui@hotmail.com>
2020-05-18 15:49:49 +1200

    Add redux state for login
    See: #64
    Co-Authored-By: Soumith C  <soumithc7@users.noreply.github.com>
    Co-Authored-By: Hideaki Murakami <hmur612@aucklanduni.ac.nz>

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,6 +1,8 @@
 import {combineReducers} from 'redux';
 import eventModal from './event-modal';
+import authentication from './authentication';
 
 export default combineReducers({
   eventModal,
+  authentication,
 });
