ca7e927ebad5e76b63214a6928c1e7263e9e4ad4
YinhuC <yinhucui@hotmail.com>
2020-05-18 15:53:12 +1200

    Add redirect to dashboard
    The home page should redirect to dashboard if logged in.
    
    Resolves: #65
    Co-Authored-By: Soumith C  <soumithc7@users.noreply.github.com>
    Co-Authored-By: Hideaki Murakami <hmur612@aucklanduni.ac.nz>

--- client/src/Main.js
+++ client/src/Main.js
@@ -1,6 +1,8 @@
 /* Third Party */
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
 
 /* Components */
 import Marketing from './Pages/MarketingPage';
@@ -20,7 +22,9 @@ class Main extends React.Component {
           <Header />
           <Switch>
             <Route exact path="/">
-              <Marketing />
+              {this.props.authenticated ?
+              <Redirect to='/landing' /> :
+                            <Marketing />}
               <Footer />
             </Route>
             <Route path="/landing">
@@ -36,4 +40,14 @@ class Main extends React.Component {
     );
   }
 }
-export default Main;
+
+Main.propTypes = {
+  authenticated: PropTypes.bool,
+};
+function mapStateToProps(state) {
+  return {
+    authenticated: state.authentication.authenticated,
+  };
+}
+
+export default connect(mapStateToProps)(Main);
