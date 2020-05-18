f48cd183c73897b2e1169eafa3baae82cf3dc732
YinhuC <yinhucui@hotmail.com>
2020-05-18 17:04:24 +1200

    Add redirect unauthenticated users
    Add loading page and logic.
    
    Resolves: #69
    Co-Authored-By: Soumith C  <soumithc7@users.noreply.github.com>
    Co-Authored-By: Hideaki Murakami <hmur612@aucklanduni.ac.nz>

--- client/src/Main.js
+++ client/src/Main.js
@@ -3,6 +3,7 @@ import React from 'react';
 import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
+import {Spinner} from 'reactstrap';
 
 /* Components */
 import Marketing from './Pages/MarketingPage';
@@ -20,21 +21,29 @@ class Main extends React.Component {
       <>
         <Router>
           <Header />
+          {this.props.loadingAuth ?
+          <div className="d-flex justify-content-center pt-5 mt-5">
+            <Spinner color="primary" style={{width: '10rem', height: '10rem'}} /> </div>:
           <Switch>
             <Route exact path="/">
-              {this.props.authenticated ?
+              {this.props.authenticated?
               <Redirect to='/landing' /> :
                             <Marketing />}
               <Footer />
             </Route>
             <Route path="/landing">
-              <Landing />
+              {this.props.authenticated?
+                <Landing /> :
+                <Redirect to='/' />}
             </Route>
-            <Route path="/calendar/:groupid" component={Calendar}/>
+            {this.props.authenticated?
+            <Route path="/calendar/:groupid" component={Calendar}/>:
+            <Route path="/calendar"><Redirect to='/' /></Route>
+            }
             <Route path="*">
               <Error />
             </Route>
-          </Switch>
+          </Switch>}
         </Router>
       </>
     );
@@ -43,10 +52,12 @@ class Main extends React.Component {
 
 Main.propTypes = {
   authenticated: PropTypes.bool,
+  loadingAuth: PropTypes.bool,
 };
 function mapStateToProps(state) {
   return {
     authenticated: state.authentication.authenticated,
+    loadingAuth: state.authentication.loadingAuth,
   };
 }
 
--- client/src/redux/reducers/authentication.js
+++ client/src/redux/reducers/authentication.js
@@ -1,10 +1,11 @@
 import {AUTHENTICATE} from '../action-types';
 
-export default function authentication(state = {authenticated: false}, action) {
+export default function authentication(state = {authenticated: false, loadingAuth: true}, action) {
   switch (action.type) {
     case AUTHENTICATE: {
       return Object.assign({}, state, {
         authenticated: action.authenticated,
+        loadingAuth: false,
       });
     }
     default:
