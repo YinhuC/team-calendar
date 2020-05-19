4d1d092460a0dcadfdde1543e8dce5c173714429
Hideaki Murakami <thehiderofkeys@gmail.com>
2020-05-19 17:39:55 +1200

    Add calendar sharing to backend
    User should be able to update the list of shared calendars
    
    Resolves: #74

--- server/api/calendars.js
+++ server/api/calendars.js
@@ -17,6 +17,30 @@ export default router => {
             })
         });
     })
+    router.post("/calendars/:_id", (req, res) => {
+        ensureLogin(req,res, async () =>{
+            isMember(req,res, async () =>{
+                const group = await Group.findOne({ "_id": req.params._id});
+                const otherCalendars = group.calendars.filter(entry => entry.googleId!==req.session.user.id );
+                const userCalendars = {googleId:req.session.user.id,calendarIds:req.body.calendars};
+                console.log(userCalendars);
+                otherCalendars.push(userCalendars);
+                group.calendars = otherCalendars;
+                group.save();
+                res.status(204).send();
+            });
+        });
+    })
+    router.get("/calendars/:_id", (req, res) => {
+        ensureLogin(req,res, async () =>{
+            isMember(req,res, async () =>{
+                const group = await Group.findOne({ "_id": req.params._id});
+                const userCalendars = group.calendars.find(entry => entry.googleId===req.session.user.id );
+                const userCalendarIds = userCalendars?userCalendars.calendarIds:[];
+                res.json({calendars:userCalendarIds});
+            });
+        });
+    })
 }
 async function ensureLogin(req, res, next) {
     if (req.session.user) {
@@ -27,7 +51,7 @@ async function ensureLogin(req, res, next) {
 }
 
 async function isMember(req, res, next) {
-    const isMember = await Group.find({ "_id": req.params.id, "members": req.session.user.id });
+    const isMember = await Group.find({ "_id": req.params._id, "members": req.session.user.id });
     if (isMember) {
         next();
     } else {
