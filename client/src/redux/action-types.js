39082434eef7864614a264610dd5f24c1ed86947
YinhuC <yinhucui@hotmail.com>
2020-05-23 21:17:09 +1200

    Add action type for set_title

--- client/src/redux/action-types.js
+++ client/src/redux/action-types.js
@@ -1,4 +1,5 @@
 export const SET_DATES = 'SET_DATES';
 export const SET_TIMES = 'SET_TIMES';
+export const SET_TITLE = 'SET_TITLE';
 export const RESET_EVENT_MODAL = 'RESET_EVENT_MODAL';
 export const AUTHENTICATE = 'AUTHENTICATE';
