86ec68ad4a6f9dd8d4250d1901a46e26f332a508
Hideaki Murakami <thehiderofkeys@gmail.com>
2020-05-24 02:27:33 +1200

    Fix positioning of modal pages on mobile

--- client/src/Pages/CalendarPage/EditModal/style.js
+++ client/src/Pages/CalendarPage/EditModal/style.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import {Modal,
 } from 'reactstrap';
 export const ModalStyled = styled(Modal)`
-  max-width:1600px;
-  width: 50%;
+  max-width:800px;
+  width: 100%;
   margin-top: 150px;
 `;
