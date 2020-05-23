6fdade445794c60b02caccdc18fbd42b5fdf3620
YinhuC <yinhucui@hotmail.com>
2020-05-23 21:18:24 +1200

    Add edit modal
    See: #106

---
+++ client/src/Pages/CalendarPage/EditModal/index.js
@@ -0,0 +1,160 @@
+/* Third Party */
+import React from 'react';
+import {
+  Row, Col, Button, ModalHeader, ModalBody, ModalFooter, FormGroup, Label, Input,
+} from 'reactstrap';
+import {ModalStyled} from './style';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {setTimes, setDates, setTitle} from '../../../redux/actions';
+import moment from 'moment';
+import {Trash2} from 'react-feather';
+
+
+class EditModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  changeInput = (event) => {
+    this.props.setTitle(
+        event.target.value,
+    );
+  }
+
+  createEvent = () => {
+    const event = {
+      summary: this.state.event,
+      start: {
+        dateTime: moment(`${this.props.startDate}-${this.props.startTime}`, 'YYYY-MM-DD-HH:mm').toISOString(),
+      },
+      end: {
+        dateTime: moment(`${this.props.endDate}-${this.props.endTime}`, 'YYYY-MM-DD-HH:mm').toISOString(),
+      },
+    };
+    fetch(`/api/calendars/${this.props.groupid}/events`, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({event: event}),
+    }).then((res, err) => {
+      if (err) {
+        console.log(err);
+      }
+      this.props.refresh();
+    });
+    this.closeModal();
+  }
+
+  closeModal = () =>{
+    this.props.toggle();
+    this.props.setTitle(
+        '',
+    );
+  }
+
+  render() {
+    return (
+      <ModalStyled size="lg" isOpen={this.props.isOpen} toggle={this.props.toggle}>
+        <ModalHeader toggle={this.props.toggle} >
+          <span style={{display: 'flex', flexWrap: 'nowrap'}}>
+            <span className="mr-1" >
+            Edit Calendar Event
+            </span>
+            <Button color="clear" style={{padding: '0', display: 'flex', alignItems: 'center'}}>
+              <Trash2 size={25}/>
+            </Button>
+          </span>
+        </ModalHeader>
+        <ModalBody>
+          <Row>
+            <Col className="col-12">
+              <FormGroup>
+                <Label>Name of Event</Label>
+                <Input
+                  type="text"
+                  name="event"
+                  placeholder="Name of event"
+                  onChange={this.changeInput}
+                  value={this.props.title}
+                />
+              </FormGroup>
+            </Col>
+            <Col>
+              <FormGroup>
+                <Label>Start Date</Label>
+                <Input
+                  type="date"
+                  name="startDate"
+                  onChange={(event) => this.props.setDates(event.target.value, this.props.endDate)}
+                  value={this.props.startDate}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label>End Date</Label>
+                <Input
+                  type="date"
+                  name="endDate"
+                  onChange={(event) => this.props.setDates(this.props.startDate, event.target.value)}
+                  value={this.props.endDate}
+                />
+              </FormGroup>
+            </Col>
+            <Col>
+              <FormGroup>
+                <Label >Start Time</Label>
+                <Input
+                  type="time"
+                  name="startTime"
+                  onChange={(event) => this.props.setTimes(event.target.value, this.props.endTime)}
+                  value={this.props.startTime}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label>End Time</Label>
+                <Input
+                  type="time"
+                  name="endTime"
+                  onChange={(event) => this.props.setTimes(this.props.startTime, event.target.value)}
+                  value={this.props.endTime}
+                />
+              </FormGroup>
+            </Col>
+          </Row>
+        </ModalBody>
+
+        <ModalFooter>
+          <Button color="primary" onClick={this.createEvent}>Save</Button>
+          <Button color="secondary" onClick={this.props.toggle}>Cancel</Button>
+        </ModalFooter>
+      </ModalStyled>);
+  }
+}
+EditModal.propTypes = {
+  toggle: PropTypes.func,
+  refresh: PropTypes.func,
+  isOpen: PropTypes.bool,
+  groupid: PropTypes.string,
+  startDate: PropTypes.string,
+  endDate: PropTypes.string,
+  startTime: PropTypes.string,
+  endTime: PropTypes.string,
+  setTimes: PropTypes.func,
+  setDates: PropTypes.func,
+  title: PropTypes.func,
+  setTitle: PropTypes.func,
+};
+function mapStateToProps(state) {
+  return {
+    startDate: state.eventModal.startDate,
+    endDate: state.eventModal.endDate,
+    startTime: state.eventModal.startTime,
+    endTime: state.eventModal.endTime,
+    title: state.eventModal.title,
+  };
+}
+const mapDispatchToProps = {
+  setTimes, setDates, setTitle,
+};
+export default connect(mapStateToProps, mapDispatchToProps)(EditModal);
--- client/src/Pages/CalendarPage/index.js
+++ client/src/Pages/CalendarPage/index.js
@@ -13,7 +13,7 @@ import InfiniteCalendar from 'react-infinite-calendar';
 import {Trash2} from 'react-feather';
 import moment from 'moment';
 import {connect} from 'react-redux';
-import {setTimes, setDates, resetEventModal} from '../../redux/actions';
+import {setTimes, setDates, setTitle, resetEventModal} from '../../redux/actions';
 
 
 // must manually import the stylesheets for each plugin
@@ -31,6 +31,7 @@ import {OuterContainer, LeftContainer, Heading,
   SmallCalendarContainer, Menu,
 } from './style';
 import EventModal from './EventModal';
+import EditModal from './EditModal';
 import MemberModal from './MemberModal';
 import randomColour from '../../Util/random-colour';
 
@@ -46,6 +47,7 @@ class CalendarPage extends React.Component {
       calendarEvents: [],
       eventModal: false,
       memberModal: false,
+      editModal: false,
       title: '',
       members: [],
       userCalendars: [],
@@ -82,6 +84,7 @@ class CalendarPage extends React.Component {
             title: event.summary,
             start: event.startDate,
             end: event.endDate,
+            editable: item.googleId === groupid,
             backgroundColor: userColour.fill,
             borderColor: userColour.fill,
             textColor: userColour.text,
@@ -182,6 +185,30 @@ class CalendarPage extends React.Component {
     });
   }
 
+  toggleEdit = () => {
+    this.setState({
+      editModal: !this.state.editModal,
+    });
+  }
+
+  openEdit = (data) => {
+    if (!data.event.durationEditable) {
+      return;
+    }
+    this.props.setDates(
+        moment(data.event.start).format('YYYY-MM-DD'),
+        moment(data.event.end).format('YYYY-MM-DD'),
+    );
+    this.props.setTimes(
+        moment(data.event.start).format('HH:mm'),
+        moment(data.event.end).format('HH:mm'),
+    );
+    this.props.setTitle(
+        data.event.title,
+    );
+    this.toggleEdit();
+  }
+
   render() {
     const {groupid} = this.props.match.params;
     const calendarsItems = [];
@@ -227,6 +254,7 @@ class CalendarPage extends React.Component {
         <EventModal isOpen={this.state.eventModal} toggle={this.toggleEventModal} groupid={groupid}
           refresh={this.triggerUpdate}/>
         <MemberModal isOpen={this.state.memberModal} toggle={this.toggleMemberModal} groupid={groupid}/>
+        <EditModal isOpen={this.state.editModal} toggle={this.toggleEdit} groupid={groupid}/>
 
         <LeftContainer>
           <Row>
@@ -295,6 +323,7 @@ class CalendarPage extends React.Component {
                     selectMirror= {true}
                     select = {this.selectCallback}
                     datesRender = {this.triggerUpdate}
+                    eventClick = {this.openEdit}
                   />
                 </CalendarContainer>
               </Col>
@@ -314,9 +343,10 @@ CalendarPage.propTypes = {
   }),
   setTimes: PropTypes.func,
   setDates: PropTypes.func,
+  setTitle: PropTypes.func,
   resetEventModal: PropTypes.func,
 };
 const mapDispatchToProps = {
-  setTimes, setDates, resetEventModal,
+  setTimes, setDates, resetEventModal, setTitle,
 };
 export default connect(null, mapDispatchToProps)(CalendarPage);
