c7604498904a75dc9383e9e14a089aa6b5ec3822
YinhuC <yinhucui@hotmail.com>
2020-05-24 20:06:57 +1200

    Add snapshot test cases

--- client/src/__tests__/test-snapshot.test.js
+++ client/src/__tests__/test-snapshot.test.js
@@ -1,10 +1,23 @@
 /* Third Party */
 import React from 'react';
-
 import renderer from 'react-test-renderer';
+
+/* Components */
 import Footer from '../Components/Footer';
+import DetailSection from '../Pages/MarketingPage/DetailSection';
+import FeatureSection from '../Pages/MarketingPage/FeatureSection';
 
 it('testing the footer', () => {
   const tree = renderer.create(<Footer/>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('testing the DetailSection', () => {
+  const tree = renderer.create(<DetailSection/>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('testing the FeatureSection', () => {
+  const tree = renderer.create(<FeatureSection/>).toJSON();
   expect(tree).toMatchSnapshot();
 });
